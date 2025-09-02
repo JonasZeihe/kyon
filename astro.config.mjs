c18bf0a1894f96288ed97a7c8ad987ae7b95a254
jonaszeihe <jonas.zeihe@gmail.com>
2025-09-02 22:44:32 +0200

    Fix stuff for initial commit and deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // astro.config.mjs
 import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   site: "https://jonaszeihe.github.io",
   base: "/kyon",
+  integrations: [mdx()]
 });
